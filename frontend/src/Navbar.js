cbb9456450fb2863b47b440ac4a50ea62794597a
Mihnea Jianu <mihneasebastian.jianu@gmail.com>
2025-12-06 17:47:25 +0200

    moved fonts in public/ folder

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -2,7 +2,6 @@ import './Navbar.css';
 
 function Navbar() {
     const links = ["/", "/", "/", "/"];
-    const LoginState = "";
 
     return (
         <nav className = "Navbar">
@@ -10,7 +9,7 @@ function Navbar() {
                 <div className = "HomePageText"> Pimp Your Grill </div>
                 <img src = "logo.png" alt = "logo" className="HomePageLogo"/>
             </a>
-            <a href = {links[1]} className = {LoginState} id = "BestGrills"> Best Grills </a>
+            <a href = {links[1]} className = "BestGrills"> Best Grills </a>
             <a href = {links[2]} className = "Login"     > Login       </a>
             <a href = {links[3]} className = "Register"  > Register    </a>
         </nav>
@@ -18,4 +17,3 @@ function Navbar() {
 }
 
 export default Navbar;
-    // <a href = {links[1]} className = "BestGrills" id = {LoginState}> Best Grills </a>
